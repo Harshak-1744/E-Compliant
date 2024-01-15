cf0c1bc06e40f515d48623589734cb9ff02aa9ae
Demo1744 <demo52688@gmail.com>
2024-01-15 11:34:02 +0530

    modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assests/logo.jpg';
 const Navbar = () => {
   return (
     <nav>
-      <div style={{ maxWidth: '1200px', margin: '0 auto', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <div style={{ maxWidth: '1200px', margin: '0 60px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
         <img src={logo} alt="Logo" style={{ height: '124px' }} className='logo' /> 
       </div>
     </nav>
