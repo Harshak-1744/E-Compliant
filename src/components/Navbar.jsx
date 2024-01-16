641b14eab7f505b233c015d5e128f7de14fc243e
Demo1744 <demo52688@gmail.com>
2024-01-16 07:24:45 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   src/components/Navbar.css 	modified:   src/components/Navbar.jsx 	new file:   src/components/pages/Login.jsx 	new file:   src/components/pages/newUser.jsx 	modified:   src/main.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import logo from '../assests/logo.jpg'; 
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
       <div className = 'navbar'>
-        <img src={logo} alt="Logo" style={{ height: '124px' }} className='logo' /> 
+        <Link to="/"><img src={logo} alt="Logo" style={{ height: '124px' }} className='logo' /></Link>
+      <ul>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+        <li>
+          <Link to="/newUser">New Register</Link>
+        </li>
+      </ul> 
       </div>
     </nav>
   );
---
+++ src/components/pages/newUser.jsx
@@ -0,0 +1,9 @@
+const newUser = () => {
+  return (
+    <div>
+        <h1>This is a New User Page</h1>
+    </div>
+  )
+}
+
+export default newUser
